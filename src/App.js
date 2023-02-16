1db505d829888dbcc46157dea0807e0bad551d85
Vittu Singh <vittu.singh@sparkeighteen.com>
2023-02-16 21:09:55 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 
-import Card from './components/Card';
-import Header from './components/Header';
-import Section from './components/Section';
-import { developerData } from './data.js';
-import { clientData } from './data.js';
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import { developerData } from "./data.js";
+import { clientData } from "./data.js";
 
 function App() {
   return (
@@ -12,41 +12,44 @@ function App() {
       <Header />
       <Section
         heading={"Our Developers"}
-        child={<>
-          {developerData.map((item) => {
-            return (
-              <>
-                <Card fullname={item.fullname} aboutme={item.aboutme} customClass={item.customClass} source={item.source} />
-
-              </>
-            );
-          })}
-        </>
+        child={
+          <>
+            {developerData.map((item) => {
+              return (
+                <>
+                  <Card
+                    fullname={item.fullname}
+                    aboutme={item.aboutme}
+                    customClass={item.customClass}
+                    source={item.source}
+                  />
+                </>
+              );
+            })}
+          </>
         }
       />
       <Section
         heading={"Our Clients"}
-        child={<>
-          {clientData.map((item) => {
-            return (
-              <>
-                <Card fullname={item.fullname} aboutme={item.aboutme} customClass={item.customClass} source={item.source} />
-
-              </>
-            );
-          })}
-        </>
+        child={
+          <>
+            {clientData.map((item) => {
+              return (
+                <>
+                  <Card
+                    fullname={item.fullname}
+                    aboutme={item.aboutme}
+                    customClass={item.customClass}
+                    source={item.source}
+                  />
+                </>
+              );
+            })}
+          </>
         }
       />
-
     </div>
-
-
   );
 }
 
 export default App;
-
-
-
-
